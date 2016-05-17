a3a531792695b876505f93c60335aae27f622d7f
chengxun <chengxun@xiaoshouyi.com>
2016-05-17 15:38:13 +0800

    增加选择控制器

--- Charts/Controllers/BeginViewController.m
+++ Charts/Controllers/BeginViewController.m
@@ -8,22 +8,61 @@
 
 #import "BeginViewController.h"
 #import "HomeViewController.h"
-@interface BeginViewController ()
-
+#import <PureLayout.h>
+@interface BeginViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic,strong)UITableView * tableView;
+@property (nonatomic,strong)NSArray * dataArray;
 @end
 
 @implementation BeginViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor yellowColor];
+    self.view.backgroundColor = [UIColor clearColor];
+    [self setUpUI];
     // Do any additional setup after loading the view.
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+- (NSArray*)dataArray{
+    if(!_dataArray){
+        _dataArray = @[@"V条形图"];
+    }
+    return _dataArray;
+}
+
+- (UITableView*)tableView{
+    if(!_tableView){
+        _tableView = [[UITableView alloc]init];
+        _tableView.dataSource = self;
+        _tableView.delegate = self;
+        [_tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"textcell"];
+        _tableView.tableFooterView = [[UIView alloc]init];
+        _tableView.rowHeight = 100;
+        [self.view addSubview:_tableView];
+    }
+    return _tableView;
+}
+
+- (void)setUpUI{
+    [self.tableView autoPinEdgesToSuperviewEdges];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return self.dataArray.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell * cell = [tableView dequeueReusableCellWithIdentifier:@"textcell"];
+    cell.textLabel.text = self.dataArray[indexPath.row];
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     HomeViewController * homeVc = [[HomeViewController alloc]init];
     [self.navigationController pushViewController:homeVc animated:YES];
 }
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
